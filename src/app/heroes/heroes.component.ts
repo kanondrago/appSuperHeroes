5b8caba586074b6decf61d539051c3ef20b4d518
kanondrago <ronaldchavezr@gmail.com>
2024-04-15 22:22:29 +0200

    Creando interfaz Hero y importando ngModel en app.module.ts

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Hero } from '../interfaces/hero'; 
 
 @Component({
   selector: 'app-heroes',
@@ -9,9 +10,18 @@ import { Component } from '@angular/core';
 
 export class HeroesComponent {
 
-  hero: string = 'Xavier';
+  hero: Hero = {
+    id: 1,
+    name: 'Xavier'
+  };
 
-  
+  constructor() {
 
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+  }
 
 }
