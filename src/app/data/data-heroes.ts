cf2e258233d2872a9ea21a612d749cee3122b008
kanondrago <ronaldchavezr@gmail.com>
2024-04-15 22:36:24 +0200

    Creando data de héroes

---
+++ src/app/data/data-heroes.ts
@@ -0,0 +1,15 @@
+
+import { Hero } from "../interfaces/hero";
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Ciclope'},
+    {id: 12, name: 'Mystique'},
+    {id: 13, name: 'Lobezno'},
+    {id: 14, name: 'Magneto'},
+    {id: 15, name: 'Jean Grey'},
+    {id: 16, name: 'Bestia'},
+    {id: 17, name: 'Tormenta'},
+    {id: 18, name: 'Júbilo'},
+    {id: 19, name: 'Bishop'},
+    {id: 20, name: 'Gambito'},
+]
